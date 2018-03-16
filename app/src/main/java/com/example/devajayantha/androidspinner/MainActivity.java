4229a2bc93693b931187963335ce974fad1897f7
deva jayantha <devajayantha870@gmail.com>
2018-03-16 22:00:04 +0800

    repair yippieww

--- app/src/main/java/com/example/devajayantha/androidspinner/MainActivity.java
+++ app/src/main/java/com/example/devajayantha/androidspinner/MainActivity.java
@@ -2,12 +2,40 @@ package com.example.devajayantha.androidspinner;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Spinner spinner_new;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        spinner_new = (Spinner) findViewById(R.id.spinner);
+        spinner_new.setOnItemSelectedListener(new CustomOnItemSelectedListener());
+    }
+
+    public class CustomOnItemSelectedListener implements AdapterView.OnItemSelectedListener {
+
+        String firstItem = String.valueOf(spinner_new.getSelectedItem());
+
+        public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+            if (firstItem.equals(String.valueOf(spinner_new.getSelectedItem()))) {
+                //memilih salah satu
+            } else {
+                Toast.makeText(parent.getContext(),
+                        "Kamu memilih : " + parent.getItemAtPosition(pos).toString(),
+                        Toast.LENGTH_LONG).show();
+            }
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> arg) {
+
+        }
     }
 }
