deb94fcec30ef33ea6373b5ce01d1f4c6a142349
deva jayantha <devajayantha870@gmail.com>
2018-03-17 11:55:42 +0800

    how to show text where click button and load to next activity yippiew

--- app/src/main/java/com/example/devajayantha/androidspinner/MainActivity.java
+++ app/src/main/java/com/example/devajayantha/androidspinner/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.devajayantha.androidspinner;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.Toast;
@@ -12,6 +14,9 @@ public class MainActivity extends AppCompatActivity {
 
     private Spinner spinner_new;
     private Button button_new;
+    private Intent intent;
+    private Object android;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,18 +26,19 @@ public class MainActivity extends AppCompatActivity {
         button_new = (Button) findViewById(R.id.Button);
 
 
+        button_new.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,SecondActivity.class);
+                intent.putExtra("data",String.valueOf(spinner_new.getSelectedItem()));
+                startActivity(intent);
+            }
+        });
         spinner_new.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                button_new.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
                         String text = spinner_new.getSelectedItem().toString();
-
                         Toast.makeText(getApplicationContext(),"Kamu memilih Kota : " + text,Toast.LENGTH_LONG).show();
-                    }
-                });
-
             }
             @Override
             public void onNothingSelected(AdapterView<?> adapterView) {
